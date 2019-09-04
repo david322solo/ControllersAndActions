565bea81b7921cec57e8408b5062fa3aca1add74
david <david.williams322solo@gmail.com>
2019-09-04 23:26:58 +0300

    basicController without code

--- ControllersAndActions/Controllers/BasicController.cs
+++ ControllersAndActions/Controllers/BasicController.cs
@@ -9,11 +9,6 @@ namespace ControllersAndActions.Controllers
 {
     public class BasicController : Controller
     {
-        public void Execute(RequestContext requestContext)
-        {
-            string controller = (string)requestContext.RouteData.Values["controller"];
-            string action = (string)requestContext.RouteData.Values["action"];
-            requestContext.HttpContext.Response.Write(string.Format("Controller: {0}, Action: {1}", controller, action));
-        }
+        
     }
 }
